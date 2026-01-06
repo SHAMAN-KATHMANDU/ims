3e7ae1dfbe31cc067b23ccd051ac78497571a5d5
Soumya-Dahal <dahalsoumya@gmail.com>
2026-01-06 16:34:20 +0545

    chore(types): add type definitions for authentication models

---
+++ apps/api/src/types/express.d.ts
@@ -0,0 +1,14 @@
+import { JwtPayload } from "jsonwebtoken";
+
+interface UserPayload extends JwtPayload {
+    id: string;
+    role: string;
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: UserPayload;
+        }
+    }
+}
