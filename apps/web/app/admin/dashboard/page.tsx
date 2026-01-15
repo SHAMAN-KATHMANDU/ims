12a3fcdf2f2ac564673448332c01087ca1c9f69f
Roshan Pandey <rpandox@gmail.com>
2026-01-15 09:43:42 +0545

    Optimize the Layout and Use Zustard for state management

--- apps/web/app/admin/dashboard/page.tsx
+++ apps/web/app/admin/dashboard/page.tsx
@@ -1,16 +1,5 @@
-import { ProtectedRoute } from "@/components/protected-route";
-import { RoleProtectedRoute } from "@/components/role-protected-route";
-import { DashboardLayout } from "@/components/dashboard-layout";
 import { HomePage } from "@/components/pages/home";
 
 export default function DashboardPage() {
-  return (
-    <ProtectedRoute>
-      <RoleProtectedRoute allowedRoles={["user", "admin", "superAdmin"]}>
-        <DashboardLayout>
-          <HomePage />
-        </DashboardLayout>
-      </RoleProtectedRoute>
-    </ProtectedRoute>
-  );
+  return <HomePage />;
 }
