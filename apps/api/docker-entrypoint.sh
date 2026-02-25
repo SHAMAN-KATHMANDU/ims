3c40d2bed9bf2e7fdcefe40dcf6a976fe9488f50
Roshan Pandey <rpandox@gmail.com>
2026-02-25 16:31:57 +0545

    fix(docker): fix the dockerfile

--- apps/api/docker-entrypoint.sh
+++ apps/api/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running database migrations..."
-node apps/api/node_modules/.bin/prisma migrate deploy --schema=apps/api/prisma/schema.prisma
+apps/api/node_modules/.bin/prisma migrate deploy --schema=apps/api/prisma/schema.prisma
 
 echo "Starting application..."
 exec node apps/api/dist/index.js
